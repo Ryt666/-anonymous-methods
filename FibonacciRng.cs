8d5fea67bc959ca8148f45d832cbcbb0f0a336f3
polina <polina@DESKTOP-5O0FJBL>
2022-06-12 12:47:46 +0300

    Добавьте файлы проекта.

---
+++ FibonacciRng.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8_2
+{
+    class FibonacciRng : IEnumerable<double>
+    {
+        private uint lagA { get; set; }
+        private uint lagB { get; set; }
+
+        public FibonacciRng(uint param1, uint param2)
+        {
+            lagA = param1;
+            lagB = param2;
+        }
+
+        public IEnumerator<double> GetEnumerator()
+        {
+            uint _max = Math.Max(lagA, lagB);
+            int indA = (int)(_max - lagA) + 1;
+            int indB = (int)(_max - lagB) + 1;
+
+            Random r = new Random(); 
+            List<double> list = new List<double>();
+            for (int i = 0; i <= _max; i++)
+                list.Add(r.NextDouble()); 
+
+            double ka = 0;
+            double kb = 0;
+            double res = 0;
+            while (true)
+            {
+                ka = list[indA];
+                kb = list[indB];
+
+                if (ka < kb)
+                    res = ka - kb + 1;
+                else
+                    res = ka - kb;
+
+                list.RemoveAt(0);
+                list.Add(res);
+
+                
+                yield return res;
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonacciRng a = new FibonacciRng(97,33);
+
+            int cnt = 0;
+            foreach (double var in a)
+            {
+                Console.WriteLine("{0:F1}",var);
+                cnt++;
+                if (cnt == 10)
+                    break;
+            }
+
+            Console.Read();
+        }
+    }
+}
